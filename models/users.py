166c6e138cff9728461405177208836f4e7f5e48
farza_mggxnah <sahandha1391@gmail.com>
2025-07-12 14:36:20 -0230

    upload

--- models/users.py
+++ models/users.py
@@ -13,7 +13,7 @@ class Users(db.Model):
 class Roles(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer,primary_key=True)
-    name = db.Column(db.Integer,unique=True,nullable=False)
+    name = db.Column(db.String(20),unique=True,nullable=False)
     description = db.Column(db.Text)
 class Permision(db.Model):
     __tablename__ = 'permisions'
